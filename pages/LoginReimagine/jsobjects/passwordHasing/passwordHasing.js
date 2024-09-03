2d8212da4d9f5fbc7507c1066936a9c7d1290739
vijay.verma <vijay.verma@innowhyte.com>
2024-09-03 05:09:30 +0000

    day start - 03-09-2024

---
+++ pages/LoginReimagine/jsobjects/passwordHasing/passwordHasing.js
@@ -0,0 +1,26 @@
+export default {
+	createHash: (password) => {
+		return dcodeIO.bcrypt.hashSync(password, 10);
+	},
+	verifyHash: (password, hash) => {
+		return dcodeIO.bcrypt.compareSync(password, hash)
+	},
+	signin: (user, password) => {
+    if (user) {
+        if (user.status === 'approved' && this.verifyHash(password, user.password_hash)) {
+            navigateTo('PostApprovalProfileCreationPag', user);
+        }
+        else if (user.status === 'appuser' && this.verifyHash(password, user.password_hash)) {
+            navigateTo('HomePageReimagine', user);
+        }else if (user.status === 'pending' && this.verifyHash(password, user.password_hash)) {
+            showAlert('Your registration is not approved, try again later', 'error');
+        }
+        else {
+            showAlert('Wrong Password', 'error');
+        }
+    }
+    else {
+        showAlert('No user with this Email, Please register', 'error');
+    }
+}
+}
